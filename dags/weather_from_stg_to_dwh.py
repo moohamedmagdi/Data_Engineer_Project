461abfa33578ee57f337f3761705fa724ea1977e
moohamedmagdi <68698665+moohamedmagdi@users.noreply.github.com>
2026-04-16 10:59:52 +0200

    Create weather_from_stg_to_dwh.py

---
+++ dags/weather_from_stg_to_dwh.py
@@ -0,0 +1,18 @@
+from airflow import DAG
+from airflow.providers.postgres.operators.postgres import PostgresOperator
+from datetime import datetime
+
+with DAG(
+    dag_id="weather_from_stg_to_dwh",
+    start_date=datetime(2024, 1, 1),
+    schedule="0 3 * * *",
+    catchup=False,
+    tags=["olist", "analytics"],
+) as dag:
+
+    create_schema = PostgresOperator(
+        task_id="create_analytics_schema",
+        postgres_conn_id="postgres_source",
+        sql="sql_weather/staging_to_analytics/00_create_schema.sql",
+    )
+
