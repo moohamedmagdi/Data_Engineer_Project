b2204dfb237e2f9fbc7f82176f3c36f2202bf968
moohamedmagdi <68698665+moohamedmagdi@users.noreply.github.com>
2026-04-17 00:23:06 +0200

    Update weather_from_raw_to_stg.py

--- dags/weather_from_raw_to_stg.py
+++ dags/weather_from_raw_to_stg.py
@@ -7,13 +7,31 @@ with DAG(
     start_date=datetime(2024, 1, 1),
     schedule="0 2 * * *",
     catchup=False,
-    tags=["weather", "etl", "staging"],
+    tags=["weather", "staging"],
 ) as dag:
 
-    # 1 - Schema
     create_schema = PostgresOperator(
         task_id="create_schema",
         postgres_conn_id="postgres_airflow",
         sql="sql_weather/raw_to_staging/00_create_schema.sql",
     )
 
+    stg_hourly = PostgresOperator(
+        task_id="stg_hourly_weather",
+        postgres_conn_id="postgres_airflow",
+        sql="sql_weather/raw_to_staging/01_stg_hourly_weather.sql",
+    )
+
+    stg_forecast = PostgresOperator(
+        task_id="stg_forecast",
+        postgres_conn_id="postgres_airflow",
+        sql="sql_weather/raw_to_staging/02_stg_forecast.sql",
+    )
+
+    stg_locations = PostgresOperator(
+        task_id="stg_locations",
+        postgres_conn_id="postgres_airflow",
+        sql="sql_weather/raw_to_staging/03_stg_locations.sql",
+    )
+
+    create_schema >> [stg_hourly, stg_forecast] >> stg_locations
