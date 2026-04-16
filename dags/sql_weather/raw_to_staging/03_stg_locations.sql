ff8dae62eab10018f05f270aec5121da6e6d996b
moohamedmagdi <68698665+moohamedmagdi@users.noreply.github.com>
2026-04-17 01:08:07 +0200

    Update 03_stg_locations.sql

--- dags/sql_weather/raw_to_staging/03_stg_locations.sql
+++ dags/sql_weather/raw_to_staging/03_stg_locations.sql
@@ -1,12 +1,19 @@
 DROP TABLE IF EXISTS staging.stg_locations;
 
 CREATE TABLE staging.stg_locations AS
-SELECT DISTINCT
-    city AS location_name,
+SELECT
+    ROW_NUMBER() OVER (ORDER BY location_name) AS location_id,
+    location_name,
     latitude,
     longitude
 FROM (
-    SELECT city, latitude, longitude FROM staging.stg_hourly_weather
-    UNION
-    SELECT city, latitude, longitude FROM staging.stg_forecast
-) t;
+    SELECT DISTINCT
+        city AS location_name,
+        latitude,
+        longitude
+    FROM (
+        SELECT city, latitude, longitude FROM staging.stg_hourly_weather
+        UNION
+        SELECT city, latitude, longitude FROM staging.stg_forecast
+    ) t
+) x;
