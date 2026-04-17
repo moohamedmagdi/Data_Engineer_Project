a6d2d7f684c9cc3f46df362efbc055fbb38cf2c8
moohamedmagdi <68698665+moohamedmagdi@users.noreply.github.com>
2026-04-17 23:52:17 +0200

    Update 02_stg_forecast.sql

--- dags/sql_weather/raw_to_staging/02_stg_forecast.sql
+++ dags/sql_weather/raw_to_staging/02_stg_forecast.sql
@@ -1,19 +1,31 @@
 DROP TABLE IF EXISTS staging.stg_forecast;
 
 CREATE TABLE staging.stg_forecast AS
-SELECT DISTINCT
-    city,
-    latitude,
-    longitude,
-    weather_time,
-    temperature,
-    windspeed,
-    humidity,
-    pressure,
-    cloudcover,
-    precipitation,
-    inserted_at
-FROM weather.forecast_5days
-WHERE 
-    temperature IS NOT NULL
-    AND weather_time >= NOW() - INTERVAL '5 days';
+SELECT *
+FROM (
+    SELECT
+        city,
+        latitude,
+        longitude,
+        weather_time,
+        temperature,
+        windspeed,
+        humidity,
+        pressure,
+        cloudcover,
+        precipitation,
+        inserted_at,
+        
+        ROW_NUMBER() OVER (
+            PARTITION BY city, latitude, longitude, weather_time
+            ORDER BY inserted_at DESC   -- الأحدث الأول
+        ) AS rn
+
+    FROM weather.forecast_5days
+
+    WHERE 
+        temperature IS NOT NULL
+        AND weather_time >= DATE_TRUNC('day', NOW())
+        AND weather_time < DATE_TRUNC('day', NOW()) + INTERVAL '5 days'
+) t
+WHERE rn = 1;
