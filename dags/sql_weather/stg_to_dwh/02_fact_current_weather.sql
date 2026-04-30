4a88d1bf6db095696b4cb93a36c7bd6c7b8c005e
moohamedmagdi <68698665+moohamedmagdi@users.noreply.github.com>
2026-05-01 00:15:47 +0300

    Update 02_fact_current_weather.sql

--- dags/sql_weather/stg_to_dwh/02_fact_current_weather.sql
+++ dags/sql_weather/stg_to_dwh/02_fact_current_weather.sql
@@ -1,17 +1,17 @@
-DROP TABLE IF EXISTS dwh.fact_current_weather;
+DROP TABLE IF EXISTS dwh.dim_date;
 
-CREATE TABLE dwh.fact_current_weather AS
-SELECT
-    ROW_NUMBER() OVER () AS current_weather_id,
-    l.location_id,
-    h.weather_time AS time,
-    h.temperature AS current_weather_temperature,
-    h.humidity AS current_weather_humidity,
-    h.windspeed AS current_weather_windspeed,
-    h.cloudcover AS current_weather_clouds,
-    h.precipitation AS current_weather_rain
-FROM staging.stg_hourly_weather h
-JOIN staging.stg_location l
-    ON h.city = l.location_name   -- ✅ نفس الفكرة
-    AND h.latitude = l.latitude
-    AND h.longitude = l.longitude;
+CREATE TABLE dwh.dim_date AS
+SELECT DISTINCT
+    ROW_NUMBER() OVER (ORDER BY date) AS date_id,
+    date,
+    EXTRACT(DAY FROM date) AS day,
+    EXTRACT(MONTH FROM date) AS month,
+    EXTRACT(YEAR FROM date) AS year,
+    EXTRACT(QUARTER FROM date) AS quarter,
+    EXTRACT(WEEK FROM date) AS week_of_year,
+    TO_CHAR(date, 'Day') AS day_name
+FROM (
+    SELECT DATE(weather_time) AS date FROM staging.stg_forecast
+    UNION
+    SELECT DATE(weather_time) AS date FROM staging.stg_hourly_weather
+) d;
