9d17363b19db2b1f73a26cb9b8a3e57f351f0522
moohamedmagdi <68698665+moohamedmagdi@users.noreply.github.com>
2026-05-01 00:16:38 +0300

    Update 03_fact_forecast.sql

--- dags/sql_weather/stg_to_dwh/03_fact_forecast.sql
+++ dags/sql_weather/stg_to_dwh/03_fact_forecast.sql
@@ -4,16 +4,20 @@ CREATE TABLE dwh.fact_forecast AS
 SELECT
     ROW_NUMBER() OVER () AS forecast_id,
     l.location_id,
+    dd.date_id,
     f.weather_time AS time,
+
     f.temperature AS forecast_temperature,
     f.humidity AS forecast_humidity,
     f.windspeed AS forecast_windspeed,
     f.cloudcover AS forecast_clouds,
     f.precipitation AS forecast_rain
+
 FROM staging.stg_forecast f
 JOIN staging.stg_location l
-    ON f.city = l.location_name   -- ✅ الصح هنا
+    ON f.city = l.location_name
     AND f.latitude = l.latitude
-    AND f.longitude = l.longitude;
+    AND f.longitude = l.longitude
 
-
+JOIN dwh.dim_date dd
+    ON DATE(f.weather_time) = dd.date;
